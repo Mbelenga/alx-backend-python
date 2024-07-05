883d19921cf7cdd08cc6d127b051ea1aafd4b484
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 07:27:58 -0700

    Updated

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+""" Duck typing annotation """
 
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """
-    Type checking
-    """
+    """ zoom """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -14,8 +13,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = (12, 72, 91)
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, int(3.0))
