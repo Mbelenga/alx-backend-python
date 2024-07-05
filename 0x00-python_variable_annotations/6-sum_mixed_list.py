fb809bc9a4ed4b54a4784405d0f8a939f8c4b815
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 13:43:56 +0300

    fixing an error

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" mixed list function """
+
+from sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    A type-annotated function which takes a list
+    of integers and floats and returns their sum
+    as a float
+    """
+    return sum(mxd_list)
