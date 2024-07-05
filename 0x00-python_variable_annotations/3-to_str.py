65ffe9dc46984d00e14c99e2e079b34c25dbb59d
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 12:53:51 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""to string function"""
+
+
+def to_str(n: float) -> str:
+    """
+    A type-annotated function that takes a float
+    as an argument and returns a string
+    representation of the float
+    """
+    return str(n)
