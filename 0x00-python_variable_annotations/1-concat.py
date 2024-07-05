5c4566c63c71a0d7e8fa5e69f1f06a2081f3725a
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 12:23:57 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This is a type-annotated functionthat returns
+    a concatenated string
+    """
+    return str1 + str2
