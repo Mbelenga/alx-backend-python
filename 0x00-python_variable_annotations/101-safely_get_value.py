4fdedf00d1d8d6ad5a8fd558565ddae87f74b827
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 16:04:38 +0300

    fixing error

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" safely_get_value function """
+
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+        default: Union[T, None]0 -> Union[Any, T]:
+        """
+        Type Annotations with TypeVar
+        """
+        if key in dct:
+            return dct[key]
+        return default
