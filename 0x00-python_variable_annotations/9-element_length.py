62ac7e03fb5e1a35d5c4d992ec357c57b0ff2c47
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 15:18:55 +0300

    an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ element_length function """
 
-from typing import#!/usr/bin/env python3
-""" element_length function """
 
 from typing import Iterable, Sequence, List, Tuple
 
