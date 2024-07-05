6f4d758a9d87a6486990d28338a32e12ce03ea2c
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 12:37:28 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+floor function
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    A type-annotated function floor which
+    takes a float n as argument
+    """
+    return math.floor(n)
