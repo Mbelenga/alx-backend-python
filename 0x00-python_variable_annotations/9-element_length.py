f688765d2edce73fa5acc02224e23d7315b602be
Mbelenga <mbelengaterence@gmail.com>
2024-07-05 15:14:18 +0300

    fixing an error

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" element_length function """
+
+from typing import#!/usr/bin/env python3
+""" element_length function """
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Annotate the functions
+    """
+    return lst
