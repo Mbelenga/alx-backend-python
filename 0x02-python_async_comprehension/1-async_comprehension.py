c10b4ba84d14af9881e3603829e43fbe986fdcb7
Mbelenga <mbelengaterence@gmail.com>
2024-07-10 18:07:04 +0300

    Updated

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 from typing import List, Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     An async generator that yields random numbers between 0 and 10.
@@ -11,6 +12,7 @@ async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
 
 async def async_comprehension() -> List[float]:
     """
